cad203a73da4d1dd8cd273eb8a97c3246332f367
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2026-01-03 09:15:03 +0545

    chore(app): added API routes for jobs and admin

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import authRoutes from "./src/routes/auth.routes.js";
 import userRoutes from "./src/routes/user.routes.js"
 import employerRoutes from "./src/routes/employer.routes.js";
+import jobRoutes from './src/routes/job.routes.js';
+import adminRoutes from './src/routes/admin.routes.js';
 import nodeCron from "node-cron";
 import { cleanUpUnverifiedUsers } from "./src/utils/cleanUpUnverifiedUsers.js";
 
@@ -18,6 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use('/api/employers', employerRoutes);
+app.use('/api/jobs', jobRoutes);
+app.use('/api/admin', adminRoutes);
 
 app.get("/", (req, res) => {
     res.send("Remittance Skill Matching API running");
