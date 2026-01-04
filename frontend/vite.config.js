7ca9f4305d300276170df5b59dabb2e3ac8ec61f
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2026-01-04 23:27:14 +0545

    chore: load env variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return defineConfig({
+    plugins: [react(), tailwindcss(),],
+    server: {
+      port: Number(env.VITE_PORT) || 5173,
+      host: env.VITE_HOST === 'true',
+      strictPort: false,
+    }
+  })
+}
