4286263d7d63799832a189109b979e25eade3d36
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2026-01-05 21:31:03 +0545

    added route for logout functionality

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loginUser, registerUser, verifyOTP } from '../controllers/auth.controller.js';
+import { loginUser, logoutUser, registerUser, verifyOTP } from '../controllers/auth.controller.js';
 import { resendOTP } from '../services/otpService.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/verify-otp', verifyOTP);
 router.post('/resend-otp', resendOTP);
+router.post('/logout', logoutUser)
 
 export default router;
