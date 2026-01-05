2a4d0ef672525834699cdd5d4b99206b81c360ed
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2026-01-05 21:23:32 +0545

    Refactor(App): remove old code and add AppRoutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,9 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+import AppRoutes from './Routes'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <div className="fixed inset-0 pointer-events-none -z-20 opacity-20">
-        <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-accent rounded-full blur-[100px] animate-pulse" />
-        <div className="absolute bottom-1/4 right-1/4 w-125 h-125 bg-secondary rounded-full blur-[150px] animate-pulse delay-700" />
-      </div>
+      <AppRoutes />
     </div>
   )
 }
