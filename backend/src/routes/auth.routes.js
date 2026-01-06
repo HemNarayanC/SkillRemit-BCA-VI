57f37d0a197882bb87c4fc9fb50e1bad3c4c0618
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2026-01-06 21:05:29 +0545

    feat(auth): add Google OAuth routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import { loginUser, logoutUser, registerUser, verifyOTP } from '../controllers/auth.controller.js';
+import passport from "../config/passport.js";
+import { googleLogin, isAuthenticated, loginUser, logoutUser, registerUser, verifyOTP } from '../controllers/auth.controller.js';
 import { resendOTP } from '../services/otpService.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
@@ -8,6 +10,13 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/verify-otp', verifyOTP);
 router.post('/resend-otp', resendOTP);
-router.post('/logout', logoutUser)
+router.post('/logout', logoutUser);
+router.get("/me", authMiddleware(), isAuthenticated);
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  googleLogin
+);
 
 export default router;
+ 
