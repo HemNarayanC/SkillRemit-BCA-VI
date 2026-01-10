faac9c5d372d2f60878696acae62fdd38b7eb247
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2026-01-10 22:39:06 +0545

    handler for forgot-password and reset password

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,13 +1,13 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { register, login, logout, isAuthenticated } from "../api/authApi";
+import { register, login, logout, isAuthenticated, resetPassword, forgotPassword } from "../api/authApi";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null); // user info
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
+  useEffect(() => {
     const checkAuth = async () => {
       try {
         const data = await isAuthenticated();
@@ -57,6 +57,30 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
+  const handleForgotPassword = async (email) => {
+    setLoading(true);
+    try {
+      const data = await forgotPassword(email);
+      setLoading(false);
+      return data; // { message: "OTP sent to your email" }
+    } catch (err) {
+      setLoading(false);
+      throw err.response?.data || { message: "Server error" };
+    }
+  };
+
+  const handleResetPassword = async ({ otp, newPassword, email }) => {
+    setLoading(true);
+    try {
+      const data = await resetPassword({ otp, newPassword, email });
+      setLoading(false);
+      return data; // { message: "Password reset successful" }
+    } catch (err) {
+      setLoading(false);
+      throw err.response?.data || { message: "Server error" };
+    }
+  };
+
   return (
     <AuthContext.Provider
       value={{
@@ -65,6 +89,8 @@ export const AuthProvider = ({ children }) => {
         register: handleRegister,
         login: handleLogin,
         logout: handleLogout,
+        forgotPassword: handleForgotPassword,
+        resetPassword: handleResetPassword
       }}
     >
       {children}
