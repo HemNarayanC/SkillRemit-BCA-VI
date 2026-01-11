e65fff995abeb207aa80aab861207c72f654d43e
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2026-01-11 19:15:43 +0545

    feat(routes): define centralized route constants

---
+++ frontend/src/constant/routes.js
@@ -0,0 +1,22 @@
+// Public routes
+const HOME_ROUTE = "/";
+
+// Protected Routes
+const DASHBOARD_ROUTE = "/dashboard";
+
+// Auth routes
+const LOGIN_ROUTE = "/auth/login";
+const REGISTER_ROUTE = "/auth/register";
+const VERIFY_OTP_ROUTE = "/auth/verify-otp";
+const OAUTH_SUCCESS_ROUTE = "/auth/oauth-success";
+const RESET_PASSWORD_ROUTE = "/auth/reset-password";
+
+export {
+  HOME_ROUTE,
+  DASHBOARD_ROUTE,
+  LOGIN_ROUTE,
+  REGISTER_ROUTE,
+  VERIFY_OTP_ROUTE,
+  OAUTH_SUCCESS_ROUTE,
+  RESET_PASSWORD_ROUTE,
+};
