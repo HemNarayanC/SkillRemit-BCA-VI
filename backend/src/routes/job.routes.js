2e136192e3d727afc878936b6a1beaf9bd785ae3
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2026-02-03 07:32:10 +0545

    routes for getting job details

--- backend/src/routes/job.routes.js
+++ backend/src/routes/job.routes.js
@@ -4,19 +4,22 @@ import {
   getEmployerJobs,
   updateJob,
   closeJob,
-  listOpenJobs
+  listOpenJobs,
+  getJobDetails
 } from '../controllers/job.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
+import { uploadJobImage } from '../config/multer.js';
 
 const router = express.Router();
 
 // Employer routes (protected)
-router.post('/', authMiddleware(['employer']), createJob);
+router.post('/', authMiddleware(['employer']), uploadJobImage.single('image'), createJob);
 router.get('/me', authMiddleware(['employer']), getEmployerJobs);
 router.put('/:job_id', authMiddleware(['employer']), updateJob);
 router.put('/:job_id/close', authMiddleware(['employer']), closeJob);
 
 // Public route to list open jobs
 router.get('/open', listOpenJobs);
+router.get('/open/:job_id', authMiddleware(['employer', 'jobseeker', 'admin']), getJobDetails);
 
 export default router;
