7b9ce41a456ad7f82b085486de6fa325e225c084
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2026-03-08 10:16:14 +0545

    chore(app): integrate all route modules with base API paths

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,13 @@ import employerRoutes from "./src/routes/employer.routes.js";
 import jobRoutes from './src/routes/job.routes.js';
 import adminRoutes from './src/routes/admin.routes.js';
 import aiRoutes from './src/routes/ai.routes.js';
+import applicationRoutes from './src/routes/applications.routes.js';
+import jobSeekerRoutes from './src/routes/jobseeker.routes.js';
+import trainerRoutes from './src/routes/trainer.routes.js'
+import aiPremiumRoutes from './src/routes/premium.routes.js'
+import enrollmentRoutes from './src/routes/enrollment.routes.js';
+import hiringRoutes from './src/routes/hiring.routes.js';
+import PlatformSettings from "./src/routes/platformSettings.routes.js";
 import nodeCron from "node-cron";
 import { cleanUpUnverifiedUsers } from "./src/utils/cleanUpUnverifiedUsers.js";
 
@@ -44,6 +51,13 @@ app.use('/api/employers', employerRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/ai', aiRoutes);
+app.use('/api/applications', applicationRoutes);
+app.use('/api/users/jobseeker', jobSeekerRoutes);
+app.use('/api/trainers', trainerRoutes);
+app.use('/api/aipremium', aiPremiumRoutes);
+app.use('/api/enrollment', enrollmentRoutes);
+app.use('/api/hiring', hiringRoutes);
+app.use('/api/admin/settings', PlatformSettings);
 
 app.get("/", (req, res) => {
     res.send("Remittance Skill Matching API running");
